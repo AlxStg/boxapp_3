6d102162b301af72b3a82f0b5bdb18756146cbcb
Joao Santiago <joao_santiago@outlook.com>
2024-02-21 02:05:24 -0300

    comecando

--- app/src/main/java/com/example/boxapp3/views/fragments/players/tv/PlayerTvChannelInfoFragment.java
+++ app/src/main/java/com/example/boxapp3/views/fragments/players/tv/PlayerTvChannelInfoFragment.java
@@ -98,6 +98,7 @@ public class PlayerTvChannelInfoFragment extends Fragment implements KeyListener
                                 @Override
                                 public void setModelToItem(PlayerTimelineItemBinding binding, EpgDb item, int bindingAdapterPosition, GenericAdapter<EpgDb, PlayerTimelineItemBinding> adapter) {
                                     binding.setModel(item);
+                                    binding.setDaysPlayback(streamXc.getTvArchiveDuration());
                                 }
                             });
                     mBinding.playerControlsTv.horizontalScrollView.setAdapter(adapter);
